227aa35e8a250bd67bfc99fab99617335b831798
Aminul Kibria <99344233+dksifatofficial@users.noreply.github.com>
2023-07-09 02:19:11 +0600

    footer

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -2,40 +2,37 @@ import Link from "next/link";
 
 function Footer() {
   return (
-    <div className="grid w-full h-auto bg-[#e7eafd]">
-      <div className="row">
-        <div className="grid place-content-center w-full md:w-6/12 md:px-0 md:py-8">
-          <p className="md:text-base">
-            Developed with love by{" "}
-            <Link className="text-[#f75023]" href="/">
-              <span className="hover:text-[#111111] hover:cursor-pointer">
-                <b>Aminul Kibria</b>
-              </span>
-            </Link>{" "}
-            © 2023
-          </p>
-        </div>
-        <div className="grid justify-center w-full md:w-6/12 md:p-0">
-          <ul
-            className="w-80 flex justify-around mt-[-1.5rem] mb-0 mx-0 p-0 
-              md:flex md:w-72 md:m-0 md:px-0 md:py-8"
+    <div className="flex flex-wrap bg-[#e7eafd] w-full justify-center py-10">
+      <div className="flex-[100%] flex flex-wrap no-underline justify-center">
+        <Link href="/termscondition">
+          <p
+            className="font-semibold text-[#414040] flex flex-wrap items-center 
+                   text-[15px] px-2.5 py-[5px] rounded-[5px] md:text-base
+                 hover:bg-[#414040] hover:text-[#e7eafd] hover:cursor-pointer"
           >
-            <li className="inline-block text-[1.063rem]">
-              <Link href="/termscondition">
-                <p className="md:text-base hover:text-[#f75023] hover:cursor-pointer">
-                  Terms & Condition
-                </p>
-              </Link>
-            </li>
-            <li className="inline-block text-[1.063rem]">
-              <Link href="/policy">
-                <p className="md:text-base hover:text-[#f75023] hover:cursor-pointer">
-                  Privacy Policy
-                </p>
-              </Link>
-            </li>
-          </ul>
-        </div>
+            Terms & Condition
+          </p>
+        </Link>
+        <Link href="/policy">
+          <p
+            className="font-semibold text-[#414040] flex flex-wrap items-center 
+                   text-[15px] px-2.5 py-[5px] rounded-[5px] md:text-base
+                 hover:bg-[#414040] hover:text-[#e7eafd] hover:cursor-pointer"
+          >
+            Privacy Policy
+          </p>
+        </Link>
+      </div>
+      <div className="w-full text-center px-2.5 py-[5px]">
+        <p className="md:text-base">
+          Compiled with NEXT-js by{" "}
+          <Link className="text-[#f75023]" href="/">
+            <span className="hover:text-[#111111] hover:cursor-pointer">
+              <b>Aminul Kibria</b>
+            </span>
+          </Link>{" "}
+          © 2023
+        </p>
       </div>
     </div>
   );

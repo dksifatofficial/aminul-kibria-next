4ee4f7daa4a6688bfb416edebb4964b32e440c31
Aminul Kibria <99344233+dksifatofficial@users.noreply.github.com>
2023-07-11 18:32:05 +0600

    change

--- app/page.js
+++ app/page.js
@@ -14,6 +14,7 @@ function Home() {
   return (
     <main >
       <TopContainer />
+       <h1>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt USD-730966598 ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</h1>
       <Qualitys />
       <AboutMe />
       <PortfolioContainer />

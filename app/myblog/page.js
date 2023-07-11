fbe7de81e54107cc5ab69b0f002df4ee7826a23b
Aminul Kibria <99344233+dksifatofficial@users.noreply.github.com>
2023-07-12 05:12:19 +0600

    off20

--- app/myblog/page.js
+++ app/myblog/page.js
@@ -1,18 +1,27 @@
 import Image from "next/image";
+import off_20 from "../assets/image/20-percent-off.png";
 import Broth_Powder from "../assets/image/Left Coast Performance Bone Broth Powder.jpg";
 
 const AmazonLink = () => {
   return (
+    <div className=" w-full grid justify-center align-middle">
     <div
-      className=" relative w-full h-auto grid bg-[#fff] dark:bg-[#000] text-2xl text-bold 
+      className=" relative w-auto h-auto grid bg-[#fff] dark:bg-[#000] text-2xl text-bold 
     align-middle justify-center mt-[100px] mb-[100px] justify-items-center"
     >
-      <p
+      <Image
+      className="absolute top-[70px] left-[-25px]"
+      height="50"
+      width="120"
+      src={off_20}
+      alt="20% off"
+      />
+      {/* <p
         className=" py-[20px] px-[20px] rounded-[100%] ring-2 ring-red-500 
       absolute top-[70px] font-bold text-red-500"
       >
         20% off
-      </p>
+      </p> */}
       <Image
         className="rounded-[10px]"
         height="400"
@@ -27,6 +36,7 @@ const AmazonLink = () => {
       >
         Diet and Sports Nutrition
       </a>
+    </div>
     </div>
   );
 };
